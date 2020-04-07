296099f842ef3f85b3bdf59c898ea9d9cc930d3e
m19e <nexus.me7290@gmail.com>
2020-04-07 19:07:21 +0900

    繰り返し文字とのマッチ*

--- main.go
+++ main.go
@@ -6,17 +6,25 @@ import (
 )
 
 func checkRegexp(reg, s string) {
-	fmt.Printf("「%s」match「%s」? =>", reg, s)
+	fmt.Printf("「%s」match「%s」? => ", reg, s)
 	fmt.Println(regexp.MustCompile(reg).Match([]byte(s)))
 }
 
 func main() {
-	// 0
+	// 0 hello
 	r := regexp.MustCompile(`golang`)
 	fmt.Println(r)
 
-	// 1
+	// 1 string
 	r1 := regexp.MustCompile(`world`)
 	fmt.Println(r1.MatchString("hello"))
 	fmt.Println(r1.MatchString("hello, world"))
+
+	// 2 repeat "*"
+	checkRegexp(`c*x`, "climax")
+	checkRegexp(`c*x`, "cx")
+	checkRegexp(`c*x`, "cccccx")
+	checkRegexp(`c*x`, "x")
+	checkRegexp(`c*x`, "climaxxxxx")
+	checkRegexp(`c*x`, "hcg")
 }
